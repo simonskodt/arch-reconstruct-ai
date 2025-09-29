9c726c6779b9f555581e43c86d672f800d07b2f6
Simon Johann Skødt <simon@skodt.dk>
2025-09-29 12:36:36 +0200

    style: remove comment

--- src/__init__.py
+++ src/__init__.py
@@ -6,6 +6,4 @@ This module exposes a simple interface to run or configure the system.
 
 __version__ = "0.1.0"
 
-# Optionally, you can import a high-level API function so users can do:
-#   from arch_reconstruct_ai import run_agent
 from .run import run_agent
