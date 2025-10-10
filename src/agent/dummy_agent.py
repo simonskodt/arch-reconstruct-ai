053b738a67bf9b4f3bd203d8fa9a73e6871fa13a
ThomasITU <thwr@itu.dk>
2025-10-10 19:35:00 +0200

    feat: add human in the loop wrapper

--- src/agent/dummy_agent.py
+++ src/agent/dummy_agent.py
@@ -4,17 +4,27 @@ This file defines a barebones agent.
 import asyncio
 from langchain.agents import create_agent
 from src.agent.tools.github import git_clone_tool
+from src.agent.tools.human_in_the_loop import add_human_in_the_loop, weather_tool
 from src.mcp.mcp_client_factory import create_mcp_client_from_config
+
 
 mcp_client = create_mcp_client_from_config()
 mcp_tools = asyncio.run(mcp_client.get_tools())
 
-tools = [git_clone_tool]
+tools = [git_clone_tool, weather_tool]
 combine_tools = tools + mcp_tools
 
-MODEL = "openai:gpt-4.1-nano"
+# MODEL = "openai:gpt-4.1-nano"
+# agent = create_agent(
+#     MODEL,
+#     tools=combine_tools,
+#     prompt="You are a helpful assistant.",
+
+# )
+print("Available tools:")
+weather_tool2 = add_human_in_the_loop(weather_tool)
 agent = create_agent(
-    MODEL,
-    tools=combine_tools,
-    prompt="You are a helpful assistant.",
+    "openai:gpt-4.1-nano",
+    tools=[weather_tool],
+    prompt="Your are a helpful assistant.",
 )
---
+++ src/agent/tools/human_in_the_loop.py
@@ -0,0 +1,70 @@
+"""A module to add human-in-the-loop review to tools."""
+from typing import Callable
+from langgraph.types import interrupt, Command
+from langchain.agents.middleware.human_in_the_loop import HumanInTheLoopConfig
+from langchain.agents.middleware.human_in_the_loop import HumanInTheLoopRequest
+from langchain_core.tools import BaseTool
+from langchain_core.runnables import RunnableConfig
+from langchain_core.tools import tool as create_tool
+
+
+@create_tool("weather_tool")
+def weather_tool(location: str) -> str:
+    """Get weather information."""
+    return f"The weather in {location} is sunny."
+
+def add_human_in_the_loop(
+    tool: Callable | BaseTool,
+    *,
+    interrupt_config: HumanInTheLoopConfig | None = None,
+) -> BaseTool:
+    """Wrap a tool to support human-in-the-loop review."""
+    if not isinstance(tool, BaseTool):
+        tool = create_tool(tool)
+
+    if interrupt_config is None:
+        interrupt_config = {
+            "allow_accept": True,
+            "allow_edit": True,
+            "allow_respond": True,
+        }
+
+    @create_tool(
+        tool.name,
+        description=tool.description,
+        args_schema=tool.args_schema
+    )
+    def call_tool_with_interrupt(config: RunnableConfig, **tool_input):
+        request: HumanInTheLoopRequest = {
+            "action_request": {
+                "action": tool.name,
+                "args": tool_input
+            },
+            "config": interrupt_config,
+            "description": "Please review the tool call below \
+            and approve, edit, or provide feedback.\n"
+            f"args_schema: {tool.args_schema}\n"
+        }
+
+        response = interrupt([request])
+
+        # approve the tool call
+        if response["type"] == "accept":
+            tool_response = tool.invoke(tool_input, config)
+        # update tool call args
+        elif response["type"] == "edit":
+            tool_input = response["args"]["args"]
+            tool_response = tool.invoke(tool_input, config)
+        # respond to the LLM with user feedback
+        elif response["type"] == "response":
+            user_feedback = response["args"]
+            tool_response = user_feedback
+        else:
+            error_msg = f"Unsupported interrupt response type: {response['type']}"
+            response = None
+            return Command(resume={"type": "response","error": error_msg})
+            # return (f"Unsupported interrupt response type: {error_msg}")
+
+        return tool_response
+
+    return call_tool_with_interrupt
