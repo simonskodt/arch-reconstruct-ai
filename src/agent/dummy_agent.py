c02730ca2c43668804b6d1fcef3b0193e3d4789d
ThomasITU <thwr@itu.dk>
2025-10-24 04:47:55 +0200

    Feat: add state management tools for agent testing state updates in ui

--- src/agent/dummy_agent.py
+++ src/agent/dummy_agent.py
@@ -4,10 +4,14 @@ This file defines a barebones agent.
 import os
 import sys
 import asyncio
+from typing import Annotated
 
 
 from langchain.agents.middleware import HumanInTheLoopMiddleware
 from langchain.agents import create_agent
+from langchain_core.messages import ToolMessage
+from langchain_core.tools import tool, InjectedToolCallId
+from langgraph.types import Command
 
 # NECESSARY: In order to enable imports from local modules
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
@@ -49,10 +53,54 @@ apply_interrupt_config_or_default(tools, DefaultInterruptConfig)
 
 tool_interrupt_configuration = create_human_in_the_loop_configuration(tools)
 
+
+@tool("update_state")
+def update_state(input_text: str, tool_call_id: Annotated[str, InjectedToolCallId]) -> Command:
+    """Update the state of the agent."""
+    return Command(update={
+        "messages": [ToolMessage(content=f"Updated state with: {input_text}",
+                                 tool_call_id=tool_call_id)],
+        "repositories": {
+            str(input_text): {
+                "path": str(input_text),
+                "url": input_text,
+            }
+        }
+    })
+
+@tool("load_state")
+def load_state(input_text: str, tool_call_id: Annotated[str, InjectedToolCallId]) -> Command:
+    """load the state of the agent."""
+    return Command(update={
+        "messages": [ToolMessage(content=f"Loaded state with: {input_text}",
+                                 tool_call_id=tool_call_id)],
+        "repositories": {
+            str(input_text): {
+                "path": str(input_text),
+                "url": input_text,
+            }
+        }
+    })
+
+@tool("load_tool_state")
+def load_tool_state(input_text: str, tool_call_id: Annotated[str, InjectedToolCallId]):
+    """load the state of the agent."""
+    return Command(update={
+        "messages": [ToolMessage(content=f"Updated state with: {input_text}",
+                                 tool_call_id=tool_call_id)],
+        "repositories": {
+            str(input_text): {
+                "path": str(input_text),
+                "url": input_text,
+            }
+        }
+    })
+
+
 MODEL = "openai:gpt-5-nano"
 agent = create_agent(
     MODEL,
-    tools=tools + [],
+    tools=[load_tool_state, update_state, git_clone_tool],
     system_prompt="Your are a helpful assistant.",
     middleware=[HumanInTheLoopMiddleware(interrupt_on=tool_interrupt_configuration),
                 PersistentPlanningMiddleware()],
